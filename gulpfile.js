4ab13a53dddbe46264b7f0f8bff14df6f6709017
xargr <xargrigoris@gmail.com>
2018-04-16 18:54:00 +0300

    logo ok

--- gulpfile.js
+++ gulpfile.js
@@ -7,41 +7,32 @@ const plugins = loadPlugins(
     }
   }
 );
-const browserSync = require('browser-sync').create();
 
-
-gulp.task('clean', () => {
-  return gulp.src('./build/css/style.css')
-    .pipe(plugins.purifycss(['./build/*.html']))
-    .pipe(gulp.dest('build/css'))
-})
-
-gulp.task('minify', () => {
-  return gulp.src('./build/css/style.css')
-    .pipe(plugins.cssmin())
+gulp.task('css-tranfer', () => {
+  return gulp.src('./src/css/*')
     .pipe(gulp.dest('build/css'))
 })
 
 
-gulp.task('js:prod', () => {
-  return gulp.src('./src/js/*')
-    .pipe(plugins.babel({
-      presets: ['env']
-    }))
-    .pipe(plugins.minify({
-      ext:{
-          src:'-debug.js',
-          min:'.js'
-      }
-  }))
+
+gulp.task('js-tranfer', () => {
+  return gulp.src(['./src/js/lightbox.js', './src/js/main.js'])
     .pipe(gulp.dest('./build/js'))
 })
+
 
 gulp.task('image:optimize', () => {
   return gulp.src(['./src/img/gallery/*', './src/img/thumb/*'])
     .pipe(plugins.imagemin())
-    .pipe(gulp.dest('./build/img/gallery'))
-    .pipe( gulp.dest('./build/img/thumb'))
+    .pipe(gulp.dest('./build/img/gallery/'))
+    .pipe( gulp.dest('./build/img/thumb/'))
+})
+
+
+gulp.task('image:transfer', () => {
+  return gulp.src('./src/images/*')
+    .pipe(plugins.imagemin())
+    .pipe(gulp.dest('./build/images/'))
 })
 
 
@@ -57,7 +48,7 @@ gulp.task('gh-page-domain', () => {
 })
 
 
-gulp.task('build', plugins.sequence(['image:optimize', 'minHtml:prod', 'gh-page-domain'], 'assets_versions:prod', 'clean', 'minify'))
+gulp.task('build', plugins.sequence(['image:optimize', 'image:transfer', 'js-tranfer', 'css-tranfer', 'minHtml:prod', 'gh-page-domain'], 'assets_versions:prod'))
 
 gulp.task('assets_versions:prod', () => {
   return gulp.src('./build/*.html')
