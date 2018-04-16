4f5503a48a406675edd6afc2c6e0fc7a4b29383b
xargr <xargrigoris@gmail.com>
2018-04-16 17:51:20 +0300

    add git ignore

--- gulpfile.js
+++ gulpfile.js
@@ -9,24 +9,6 @@ const plugins = loadPlugins(
 );
 const browserSync = require('browser-sync').create();
 
-gulp.task('sass:dev', () => {
-  return gulp.src('./src/sass/*.scss')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./local/css'))
-    .pipe(browserSync.stream())
-})
-
-gulp.task('sass:prod', () => {
-  return gulp.src('./src/sass/*.scss')
-    .pipe(plugins.sass.sync().on('error', plugins.sass.logError))
-    .pipe(plugins.autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('./build/css'))
-})
 
 gulp.task('clean', () => {
   return gulp.src('./build/css/style.css')
@@ -40,16 +22,6 @@ gulp.task('minify', () => {
     .pipe(gulp.dest('build/css'))
 })
 
-gulp.task('js:dev', () => {
-  return gulp.src('./src/js/*')
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.babel({
-        presets: ['env']
-    }))
-    .pipe(plugins.sourcemaps.write())
-    .pipe(gulp.dest('./local/js'))
-    .pipe(browserSync.stream())
-})
 
 gulp.task('js:prod', () => {
   return gulp.src('./src/js/*')
@@ -66,16 +38,12 @@ gulp.task('js:prod', () => {
 })
 
 gulp.task('image:optimize', () => {
-  return gulp.src('./src/img/*')
+  return gulp.src(['./src/img/gallery/*', './src/img/thumb/*'])
     .pipe(plugins.imagemin())
-    .pipe(gulp.dest('./build/img'))
+    .pipe(gulp.dest('./build/img/gallery'))
+    .pipe( gulp.dest('./build/img/thumb'))
 })
 
-gulp.task('minHtml:dev', () => {
-  return gulp.src('./src/*.html')
-  .pipe(gulp.dest('./local'))
-  .pipe(browserSync.stream())
-})
 
 gulp.task('minHtml:prod', () => {
   return gulp.src('./src/*.html')
@@ -88,19 +56,8 @@ gulp.task('gh-page-domain', () => {
     .pipe(gulp.dest('build'))
 })
 
-gulp.task('default', () => {
-  browserSync.init({
-    server: {
-        baseDir: "local/"
-    }
-  })
 
-  gulp.watch('./src/sass/*.scss', ['sass:dev']).on('change', browserSync.reload);
-  gulp.watch('./src/*.html', ['minHtml:dev']).on('change', browserSync.reload);
-  gulp.watch('./src/js/*.js', ['js:dev']).on('change', browserSync.reload);
-})
-
-gulp.task('build', plugins.sequence(['sass:prod', 'js:prod', 'image:optimize', 'minHtml:prod', 'gh-page-domain'], 'assets_versions:prod', 'clean', 'minify'))
+gulp.task('build', plugins.sequence(['image:optimize', 'minHtml:prod', 'gh-page-domain'], 'assets_versions:prod', 'clean', 'minify'))
 
 gulp.task('assets_versions:prod', () => {
   return gulp.src('./build/*.html')
